342729135f077b9d5c817ad26de82899ac6e7429
ZubairAhmad001 <118602652+ZubairAhmad001@users.noreply.github.com>
2024-07-24 23:00:31 +0530

    Delete AdditionTwoNumbers.java

--- AdditionTwoNumbers.java
+++
@@ -1,9 +0,0 @@
-public class AdditionTwoNumbers
-{
-    public static void main (String[] args){
-        int a = 5;
-        float b = 7.5f;
-        float result = a+b;
-        System.out.println(result);
-    }
-}
